53bd100a8fc7dbe3910b8befb2c0896097c49f64
Damian Polak <damianpolak.it@gmail.com>
2023-07-25 21:38:20 +0200

    Auto-generated commit [ci skip]

--- src/app/core/interfaces/board.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Grid {
-  row: number,
-  col: number
-}
-
-export interface TestCase extends Record<string, Grid> { }
--- src/app/core/interfaces/score.interface.ts
+++
@@ -1,9 +0,0 @@
-import { LEVELS } from "./global.interface";
-
-export interface Score {
-  type: 'WIN' | 'LOSE',
-  level: LEVELS;
-  time: number;
-  scorePerc: number;
-  timestamp: Date
-}
